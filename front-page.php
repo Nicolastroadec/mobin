2c9150cd48abea0d729a6fcfe2836e01a7f13823
nicolastroadec <troadecnicolas@hotmail.fr>
2024-03-15 16:45:46 +0100

    header fix

--- front-page.php
+++ front-page.php
@@ -15,10 +15,19 @@
  */
 
 get_header();
+$id = get_the_ID();
+$premiere_phrase_titre = get_field('premiere_phrase_titre', $id);
+$deuxieme_phrase_titre = get_field('deuxieme_phrase_titre', $id);
+
+
 ?>
 <div class="hero">
     <?php $image_mise_en_avant = get_the_post_thumbnail_url(); ?>
     <img src="<?= $image_mise_en_avant; ?>" alt="<?= get_the_title(); ?>">
+    <h1 class="internal-section-width">
+        <span><?= $premiere_phrase_titre ?></span>
+        <span><?= $deuxieme_phrase_titre ?></span>
+    </h1>
 </div>
 <?php require_once(get_template_directory() . '/inc/inc-articles.php'); ?>
 
