e0772f82b16288a1727a0f489de9e8968c200239
nicolastroadec <troadecnicolas@hotmail.fr>
2024-03-19 09:36:20 +0100

    register script

--- functions.php
+++ functions.php
@@ -11,7 +11,15 @@ require_once get_template_directory() . '/functions/custom-class-menu.php';
 
 require_once get_template_directory() . '/functions/add-menu-support-and-register-menus.php';
 
-wp_enqueue_script('mobin-script', get_template_directory_uri() . '/js/map.js', array('jquery'), '1.0.0', true);
+
+function registerMapScript()
+{
+    wp_enqueue_script('mobin-script', get_template_directory_uri() . '/js/map.js', array('jquery'), '1.0.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'registerMapScript');
+
+
 
 
 
