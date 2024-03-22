9aad77f9b5acd7a5a6408f740c4d2ac692fa769b
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-03-22 11:30:11 +0100

    update

--- inc/inc-map-region.php
+++ inc/inc-map-region.php
@@ -7,7 +7,7 @@
 
         <h2>0 - MapBox</h2><br><br>
 
-        <img id="map11" src="https://api.mapbox.com/styles/v1/mapbox/light-v11/static/geojson(%7B%22type%22%3A%22FeatureCollection%22%2C%22features%22%3A%5B%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22marker-size%22%3A%22small%22%2C%22marker-symbol%22%3A%22airport%22%2C%22marker-color%22%3A%22%230000FF%22%7D%2C%22geometry%22%3A%7B%22type%22%3A%22MultiPoint%22%2C%22coordinates%22%3A%5B%5B-1.6778%2C48.1173%5D%2C%5B-4.486%2C48.3904%5D%5D%7D%7D%5D%7D)/-2.8808,48.1227,7 ,0/1000x500?access_token=pk.eyJ1Ijoibmljb2xhc3Ryb2FkZWMiLCJhIjoiY2x1MTBrOTR5MGV5eDJsbjBrdGRrcWNyNyJ9.7m60Z52KxMHCajM-fbksNw" alt="" />
+        <img class="map" id="map11" src="https://api.mapbox.com/styles/v1/mapbox/light-v11/static/geojson(%7B%22type%22%3A%22FeatureCollection%22%2C%22features%22%3A%5B%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22marker-size%22%3A%22small%22%2C%22marker-symbol%22%3A%22airport%22%2C%22marker-color%22%3A%22%230000FF%22%7D%2C%22geometry%22%3A%7B%22type%22%3A%22MultiPoint%22%2C%22coordinates%22%3A%5B%5B-1.6778%2C48.1173%5D%2C%5B-4.486%2C48.3904%5D%5D%7D%7D%5D%7D)/-2.8808,48.1227,7 ,0/1000x500?access_token=pk.eyJ1Ijoibmljb2xhc3Ryb2FkZWMiLCJhIjoiY2x1MTBrOTR5MGV5eDJsbjBrdGRrcWNyNyJ9.7m60Z52KxMHCajM-fbksNw" alt="" />
 
         <div class="map" id="map" style="height: 500px; width: 1000px"></div>
         <h2>2 - Carto</h2><br><br>
